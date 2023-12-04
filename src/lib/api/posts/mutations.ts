341e19527b1c3c8160eacad9ea42a23afd4652de
congtrieu98 <trieunguyen2806@gmail.com>
2023-12-04 23:35:57 +0700

    test send email

--- src/lib/api/posts/mutations.ts
+++ src/lib/api/posts/mutations.ts
@@ -11,61 +11,81 @@ import { getUserAuth } from "@/lib/auth/utils";
 import { resend } from "@/lib/email";
 import { EmailPost } from "@/components/emails/EmailPost";
 
-// export const createPost = async (post: NewPostParams) => {
-//   const { session } = await getUserAuth();
-//   const newPost = insertPostSchema.parse({ ...post, userId: session?.user.id! });
-//   try {
-//     // @ts-ignore
-//     const p = await db.post.create({ data: newPost });
-//     return { post: p };
-//   } catch (err) {
-//     const message = (err as Error).message ?? "Error, please try again";
-//     console.error(message);
-//     return { error: message };
-//   }
-// };
-
 export const createPost = async (post: NewPostParams) => {
   const { session } = await getUserAuth();
   const newPost = insertPostSchema.parse({ ...post, userId: session?.user.id! });
-
-  // @ts-ignore
-  const prisma = new PrismaClient().$extends({
-    query: {
-      post: {
-        async create() {
-          const { session } = await getUserAuth();
-          const newPost = insertPostSchema.parse({ ...post, userId: session?.user.id! });
-          try {
-            const p = await db.post.create({ data: newPost });
-            if (p) {
-              const user = await db.user.findMany({ where: { id: session?.user.id }, include: { followers: true } });
-              if (user) {
-                user.forEach(async (follower: any) => {
-                  const { name, email } = follower;
-                  await resend.emails.send({
-                    from: "Kirimase <onboarding@resend.dev>",
-                    to: [email],
-                    subject: `Hello ${name}!`,
-                    // @ts-ignore
-                    react: EmailPost({ name: name, post: p }),
-                    text: "Email powered by Resend.",
-                  });
-                });
-              }
-            }
-            return { post: p };
-          } catch (err) {
-            const message = (err as Error).message ?? "Error, please try again";
-            console.error(message);
-            return { error: message };
-          }
-        },
-      },
-    },
-  });
-  await prisma.post.create({ data: newPost })
+  try {
+    // @ts-ignore
+    const p = await db.post.create({ data: newPost });
+    const users = await db.user.findMany({ where: { id: session?.user.id }, include: { followers: true, follows: true } });
+    console.log(users)
+    console.log(users[0].followers)
+    console.log(users[0].follows)
+    if (users) {
+      users.forEach(async (user: any) => {
+        // TODO
+        // Lúc follow add đang sai logic dẫn đến gửi email bị sai
+        const { name, email } = user;
+        // await resend.emails.send({
+        //   from: "Kirimase <onboarding@resend.dev>",
+        //   to: [email],
+        //   subject: `Hello ${name}!`,
+        //   // @ts-ignore
+        //   react: EmailPost({ name: name, post: p }),
+        //   text: "Email powered by Resend.",
+        // });
+      });
+    }
+    return { post: p };
+  } catch (err) {
+    const message = (err as Error).message ?? "Error, please try again";
+    console.error(message);
+    return { error: message };
+  }
 };
+
+// export const createPosts = async (post: NewPostParams) => {
+//   const { session } = await getUserAuth();
+//   const newPost = insertPostSchema.parse({ ...post, userId: session?.user.id! });
+//   // @ts-ignore
+//   const prisma = new PrismaClient().$extends({
+//     query: {
+//       post: {
+//         async create() {
+//           const { session } = await getUserAuth();
+//           console.log("session user:", session)
+//           const newPost = insertPostSchema.parse({ ...post, userId: session?.user.id! });
+//           try {
+//             const p = await db.post.create({ data: newPost });
+//             if (p) {
+//               console.log("data baif posst:", p)
+//               const user = await db.user.findMany({ where: { id: session?.user.id }, include: { followers: true } });
+//               if (user) {
+//                 user.forEach(async (follower: any) => {
+//                   const { name, email } = follower;
+//                   await resend.emails.send({
+//                     from: "Kirimase <onboarding@resend.dev>",
+//                     to: [email],
+//                     subject: `Hello ${name}!`,
+//                     // @ts-ignore
+//                     react: EmailPost({ name: name, post: p }),
+//                     text: "Email powered by Resend.",
+//                   });
+//                 });
+//               }
+//             }
+//             return { post: p };
+//           } catch (err) {
+//             const message = (err as Error).message ?? "Error, please try again";
+//             console.error(message);
+//             return { error: message };
+//           }
+//         },
+//       },
+//     },
+//   });
+//   await prisma.post.create({ data: newPost })
+// };
 
 export const updatePost = async (id: PostId, post: UpdatePostParams) => {
   const { session } = await getUserAuth();
@@ -74,7 +94,6 @@ export const updatePost = async (id: PostId, post: UpdatePostParams) => {
   try {
     // @ts-ignore
     const p = await db.post.update({ where: { id: postId, }, data: newPost })
-    console.log('newPost:', p)
     return { post: p };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
